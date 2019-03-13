99b910001a41d18405770ab79408b2a6981784eb
pkrueger <philip.krueger@caci.com>
2019-03-13 09:48:01 -0700

    Part 3

--- ComplexNumber/Complex.cs
+++ ComplexNumber/Complex.cs
@@ -4,6 +4,8 @@ public class Complex
 {
   private double rP = 0; // real part
   private double iP = 0; // imaginary part
+  private double polarMagnitude = 0;
+  private double polarAngle = 0;
   //tracks if the complex object is undefined
   private bool isUndefined = false;
 
@@ -32,30 +34,8 @@ public class Complex
   {
     rP = r;
     iP = i;
-  }
-
-  public Complex add(Complex c1)
-  {
-    //check if this complex number or the other complex number is undefined
-    //if either are undefined, then return an undefined complex number
-    if (isUndefined || c1.isUndefined)
-      return new Complex(null);
-    var realPart = rP + c1.rP;
-    var imaginaryPart = iP + c1.iP;
-    //return a new complex number to prevent mutation of other two complex numbers
-    return new Complex(realPart, imaginaryPart);
-  }
-
-  public static Complex addTwo(Complex c1, Complex c2)
-  {
-    //check if this complex number or the other complex number is undefined
-    //if either are undefined, then return an undefined complex number
-    if (c1.isUndefined || c2.isUndefined)
-      return new Complex(null);
-    var realPart = c1.rP + c2.rP;
-    var imaginaryPart = c1.iP + c2.iP;
-    //return a new complex number to prevent mutation of other two complex numbers
-    return new Complex(realPart, imaginaryPart);
+    polarMagnitude = Math.Sqrt(rP * rP + iP * iP);
+    polarAngle = Math.Atan(iP / rP) * (180 / Math.PI);
   }
 
   public Complex sub(Complex c1)
@@ -82,32 +62,6 @@ public class Complex
     return new Complex(realPart, imaginaryPart);
   }
 
-  public Complex mul(Complex c1)
-  {
-    //check if this complex number or the other complex number is undefined
-    //if either are undefined, then return an undefined complex number
-    if (isUndefined || c1.isUndefined)
-      return new Complex(null);
-    // use the formula (a+bj) * (c+dj) = (ac-bd) + (ad+bc)j
-    var realPart = rP * c1.rP - iP * c1.iP;
-    var imaginaryPart = rP * c1.iP + iP * c1.rP;
-    //return a new complex number to prevent mutation of other two complex numbers
-    return new Complex(realPart, imaginaryPart);
-  }
-
-  public static Complex mulTwo(Complex c1, Complex c2)
-  {
-    //check if this complex number or the other complex number is undefined
-    //if either are undefined, then return an undefined complex number
-    if (c1.isUndefined || c2.isUndefined)
-      return new Complex(null);
-    // use the formula (a+bj) * (c+dj) = (ac-bd) + (ad+bc)j
-    var realPart = c1.rP * c2.rP - c1.iP * c2.iP;
-    var imaginaryPart = c1.rP * c2.iP + c1.iP * c2.rP;
-    //return a new complex number to prevent mutation of other two complex numbers
-    return new Complex(realPart, imaginaryPart);
-  }
-
   public Complex div(Complex c1)
   {
     //check if this complex number or the other complex number is undefined
@@ -115,8 +69,8 @@ public class Complex
     if (isUndefined || c1.isUndefined || isZero(c1))
       return new Complex(null);
     // use the formula (a+bj) / (c+dj) = (1/(c^2 + d^2)) * (ac+bd) + (1/(c^2 + d^2)) * (bc - ad)j
-    var realPart = (1 / (c1.rP * c1.rP + c1.iP * c1.iP))*(rP * c1.rP + iP * c1.iP);
-    var imaginaryPart = (1 / (c1.rP * c1.rP + c1.iP * c1.iP))*(iP * c1.rP - rP * c1.iP);
+    var realPart = (1 / (c1.rP * c1.rP + c1.iP * c1.iP)) * (rP * c1.rP + iP * c1.iP);
+    var imaginaryPart = (1 / (c1.rP * c1.rP + c1.iP * c1.iP)) * (iP * c1.rP - rP * c1.iP);
     //return a new complex number to prevent mutation of other two complex numbers
     return new Complex(realPart, imaginaryPart);
   }
@@ -128,8 +82,8 @@ public class Complex
     if (c1.isUndefined || c2.isUndefined || isZero(c2))
       return new Complex(null);
     // use the formula (a+bj) / (c+dj) = (1/(c^2 + d^2)) * (ac+bd) + (1/(c^2 + d^2)) * (bc - ad)j
-    var realPart = (1 / (c2.rP * c2.rP + c2.iP * c2.iP))*(c1.rP * c2.rP + c1.iP * c2.iP);
-    var imaginaryPart = (1 / (c2.rP * c2.rP + c2.iP * c2.iP))*(c1.iP * c2.rP - c1.rP * c2.iP);
+    var realPart = (1 / (c2.rP * c2.rP + c2.iP * c2.iP)) * (c1.rP * c2.rP + c1.iP * c2.iP);
+    var imaginaryPart = (1 / (c2.rP * c2.rP + c2.iP * c2.iP)) * (c1.iP * c2.rP - c1.rP * c2.iP);
     //return a new complex number to prevent mutation of other two complex numbers
     return new Complex(realPart, imaginaryPart);
   }
@@ -139,12 +93,22 @@ public class Complex
     return c.rP == 0 && c.iP == 0;
   }
 
-  public void print()
+  public Complex print()
   {
     if (isUndefined)
       Console.WriteLine("Undefined");
     else
       Console.WriteLine("{0} + {1}j", rP, iP);
+    return this;
+  }
+
+  public Complex printPolar()
+  {
+    if (isUndefined)
+      Console.WriteLine("Undefined");
+    else
+      Console.WriteLine(polarMagnitude + "e^(j" + polarAngle + ")");
+    return this;
   }
 
 }
